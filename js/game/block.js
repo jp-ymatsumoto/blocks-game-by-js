13e0346d0db871e636a2c197d3ef2bac50acb9f4
jp-ymatsumoto <y.matsumoto@iwate-designer.com>
2024-04-30 05:26:53 +0900

    Add: HardBlockクラスの実装

--- js/game/block.js
+++ js/game/block.js
@@ -23,11 +23,41 @@ export class Block {
     this.height = height;
   }
 
+  /** 得点を取得する */
+  getPoint() {
+    return Block.POINT;
+  }
+
   /** 描画する */
   draw() {
     if (this.status === true) {
       // ブロックを描画する
       this.context.fillStyle = "#A47F61";
+      this.context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+}
+
+export class HardBlock extends Block {
+  /** 得点 */
+  static POINT = 50;
+  /** HP */
+  hp = 3;
+
+  constructor(context, x, y, width, height) {
+    super(context, x, y, width, height);
+  }
+
+  /** 得点を取得する */
+  getPoint() {
+    return HardBlock.POINT;
+  }
+
+  /** 描画する */
+  draw() {
+    if (this.status === true) {
+      // ブロックを描画する
+      this.context.fillStyle = "#D2691E";
       this.context.fillRect(this.x, this.y, this.width, this.height);
     }
   }
